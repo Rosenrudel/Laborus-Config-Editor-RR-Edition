24d037b6edcc9f095aaba4e2ed5e67f69e2dce72
Lergin <lergin@gmx.de>
2017-05-03 16:52:21 +0200

    release version

---
+++ public/dist/js/data.js
@@ -0,0 +1,11 @@
+
+var data = {
+	logging: {},
+	fallbackLanguage:'en',
+	enabledGamemodes:["Survival", "Adventure", "nothing"],
+	antiReplaceTime:48,
+	antiReplaceActive:false,
+	maxSelectedJobs:1,
+	xpWithoutJob:0.5,
+	jobs:[]
+};
